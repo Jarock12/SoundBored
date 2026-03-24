799292c799e677a4b728433e0d7b279347cc0ccc
Jarock12 <josh.arockiasamy@outlook.com>
2026-03-24 06:06:54 -0400

    removed duplicate files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['i.scdn.co'], // For Spotify images
-  },
-}
-
-module.exports = nextConfig
